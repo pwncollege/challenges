9b5b273ed8ca77af5b2a7dd6d7b5347c0e05c56e
Yan <yans@yancomm.net>
2025-09-05 15:19:44 -0700

    auth bypass

---
+++ web_security/auth-bypass-param/tests_public/test_functionality.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import subprocess
+import requests
+import atexit
+import time
+import os
+
+FLAG = os.environ["FLAG"]
+
+proc = subprocess.Popen(['/challenge/server'])
+atexit.register(lambda: (proc.terminate(), proc.wait()))
+time.sleep(1)  # Give it time to start
+
+response = requests.get("http://challenge.localhost/")
+assert response.status_code == 200
+assert "Welcome to the login service" in response.text
+
+response = requests.post("http://challenge.localhost/", data={
+    "username": "guest",
+    "password": "password"
+})
+assert response.status_code == 200
+assert response.history[0].status_code == 302
+assert "session_user=guest" in response.url
+assert "Hello, guest!" in response.text
+assert FLAG not in response.text
+
+response = requests.post("http://challenge.localhost/", data={
+    "username": "admin",
+    "password": "wrong"
+})
+assert response.status_code == 403
+assert "Invalid username or password" in response.text
+
+print("Public tests passed!")
