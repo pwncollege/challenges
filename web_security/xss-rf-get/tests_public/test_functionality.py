464e600f26cf3a042fc08c907b25855c19df7e55
Yan <yans@yancomm.net>
2025-09-05 15:49:38 -0700

    git commit -m xss

---
+++ web_security/xss-rf-get/tests_public/test_functionality.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import subprocess
+import requests
+import atexit
+import time
+import os
+
+FLAG = os.environ["FLAG"]
+
+proc = subprocess.Popen(['/challenge/server'])
+atexit.register(lambda: (proc.terminate(), proc.wait()))
+time.sleep(1)  # Give it time to start
+
+# Test that server is running
+response = requests.get("http://challenge.localhost/")
+assert response.status_code == 200
+assert "pwnpost" in response.text
+
+print("Public tests passed!")
