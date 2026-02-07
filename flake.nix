60aa9f2ea6a8b5f1183de49fa7ffffd0839f77c3
Connor Nelson <Connor@ConnorNelson.com>
2026-02-07 10:13:24 -0700

    runtime: add kata runtime (#90)
    Adds a Nix flake dev shell that starts a dedicated, socket-activated
    Docker daemon with Kata Containers enabled.
    
    Usage:
    
    ```bash
    nix develop
    
    docker run --rm --runtime=kata alpine:3.20 uname -r
    
    docker run --rm --runtime=kata \
      --annotation io.katacontainers.config.hypervisor.default_memory=4096 \
      alpine:3.20 free -h
    ```
    
    Notes:
    - Does not require NixOS modules; uses systemd runtime units.
    - Uses `/var/lib/pwn.college/docker` for Docker data (shared across
    devs) and `/run/pwn.college/docker/docker.sock` for the socket.

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "pwn.college challenges dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      systems = [ "x86_64-linux" ];
+      forAllSystems = f: lib.genAttrs systems (system: f system);
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+
+          pwn-challenge-runtime = import ./runtime { inherit pkgs lib; };
+
+          pwnshop = pkgs.writeShellApplication {
+            name = "pwnshop";
+            runtimeInputs = with pkgs; [
+              git
+              uv
+            ];
+            text = ''
+              set -euo pipefail
+              root="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
+              exec "$root/pwnshop" "$@"
+            '';
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              docker
+              git
+              git-crypt
+              pwn-challenge-runtime
+              pwnshop
+              uv
+            ];
+            shellHook = ''
+              export DOCKER_HOST="$(sudo ${pwn-challenge-runtime}/bin/pwn-challenge-runtime)"
+            '';
+          };
+        });
+    };
+}
---
+++ runtime/kernel.nix
@@ -0,0 +1,97 @@
+{ pkgs, name }:
+let
+  kataContainersSrc = pkgs.fetchzip {
+    url = "https://github.com/kata-containers/kata-containers/archive/refs/tags/${pkgs.kata-runtime.version}.tar.gz";
+    hash = "sha256-+SppAF77NbXlSrBGvIm40AmNC12GrexbX7fAPBoDAcs=";
+  };
+
+  kernelVersion = builtins.readFile "${pkgs.runCommand "kata-kernel-version" { nativeBuildInputs = [ pkgs.yq ]; } ''
+    version="$(yq -r '.assets.kernel.version' ${kataContainersSrc}/versions.yaml)"
+    printf '%s' "''${version#v}" > "$out"
+  ''}";
+  kernelMajor = builtins.elemAt (pkgs.lib.splitString "." kernelVersion) 0;
+  kernelTarball = pkgs.fetchurl {
+    url = "http://cdn.kernel.org/pub/linux/kernel/v${kernelMajor}.x/linux-${kernelVersion}.tar.xz";
+    hash = "sha256-q0iACrSZhaeNIxiuisXyj9PhI+oXNX7yFJgQWlMzczY=";
+  };
+
+  config =
+    let
+      enable = [
+        "SECURITY_LANDLOCK"
+        "BPF_JIT"
+        "BPF_SYSCALL"
+        "BPF"
+        "DEBUG_KERNEL"
+        "DEBUG_INFO_DWARF4"
+        "DEBUG_INFO_BTF"
+        "DYNAMIC_FTRACE"
+        "FTRACE"
+        "FUNCTION_TRACER"
+        "KPROBE_EVENTS"
+        "KPROBES"
+        "PERF_EVENTS"
+        "PROFILING"
+      ];
+    in
+    pkgs.writeText "${name}.conf" (pkgs.lib.strings.concatLines (map (t: "CONFIG_${t}=y") enable));
+in
+pkgs.stdenv.mkDerivation {
+  pname = "${name}-linux-kernel";
+  version = kernelVersion;
+  dontUnpack = true;
+  buildInputs = with pkgs; [
+    zlib
+  ];
+  nativeBuildInputs = with pkgs; [
+    bc
+    bison
+    coreutils
+    cpio
+    elfutils
+    flex
+    gawk
+    gcc
+    gnumake
+    gnugrep
+    gnutar
+    gzip
+    kmod
+    ncurses
+    openssl
+    pahole
+    perl
+    pkg-config
+    python3
+    rsync
+    util-linux
+    xz
+  ];
+  buildPhase = ''
+    runHook preBuild
+
+    export SOURCE_DATE_EPOCH=1
+    export KBUILD_BUILD_TIMESTAMP="1970-01-01T00:00:01Z"
+    export KBUILD_BUILD_USER="nix"
+    export KBUILD_BUILD_HOST="nix"
+
+    mkdir -p tools
+    cp -R ${kataContainersSrc}/tools/packaging tools/packaging
+    chmod -R u+w tools/packaging
+    patchShebangs tools/packaging
+    cd tools/packaging/kernel
+
+    install -D -m 0644 ${config} configs/fragments/x86_64/${name}.conf
+
+    cp ${kernelTarball} linux-${kernelVersion}.tar.xz
+    sha256sum linux-${kernelVersion}.tar.xz > linux-${kernelVersion}.tar.xz.sha256
+
+    DESTDIR="$out" PREFIX="/" ./build-kernel.sh -v "${kernelVersion}" setup
+    patchShebangs kata-linux-${kernelVersion}-*/scripts kata-linux-${kernelVersion}-*/tools
+    DESTDIR="$out" PREFIX="/" ./build-kernel.sh -v "${kernelVersion}" build
+    DESTDIR="$out" PREFIX="/" ./build-kernel.sh -v "${kernelVersion}" install
+
+    runHook postBuild
+  '';
+  installPhase = "true";
+}
