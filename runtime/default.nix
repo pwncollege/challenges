5a6ed6b6c0ae255a3966f23a9ce2d3087f204d5d
Connor Nelson <Connor@ConnorNelson.com>
2026-02-17 19:13:53 +0000

    runtime: gcroot runtime script (keep units/configs)

--- runtime/default.nix
+++ runtime/default.nix
@@ -166,7 +166,8 @@ pkgs.writeShellApplication {
     ln -sfn "${containerdServiceUnit}" "/run/systemd/system/$containerd_service_unit"
 
     mkdir -p /nix/var/nix/gcroots
-    ln -sfn "${dockerSystemdServiceUnit}" "/nix/var/nix/gcroots/${name}"
+    runtime_store_path="$(readlink -f "$0" 2>/dev/null || echo "$0")"
+    ln -sfn "$runtime_store_path" "/nix/var/nix/gcroots/${name}"
 
     systemctl daemon-reload
 
