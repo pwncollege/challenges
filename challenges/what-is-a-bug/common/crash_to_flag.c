c8cacccb51264af9dcf2c535eb0cab794e1ae0fc
Connor Nelson <Connor@ConnorNelson.com>
2026-02-24 19:23:22 +0000

    what-is-a-bug: tighten flag checks and harden crash paths

--- challenges/what-is-a-bug/common/crash_to_flag.c
+++ challenges/what-is-a-bug/common/crash_to_flag.c
@@ -1,44 +1,42 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <signal.h>
-#include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include <stdio.h>
 
 void handle_crash(int signum)
 {
-	puts("You crashed it! Here is your flag:");
-	char flag[1024] = {0};
+	(void)signum;
+
+	// If the process retained saved root IDs, regain euid/egid before reading /flag.
+	setresgid(-1, 0, -1);
+	setresuid(-1, 0, -1);
+
+	write(STDOUT_FILENO, "You crashed it! Here is your flag:\n", 35);
+
+	char flag[1024];
+	memset(flag, 0, sizeof(flag));
 	int fd = open("/flag", O_RDONLY);
-	if (fd < 0)
-	{
-		perror("unable to open /flag");
-		exit(1);
+	if (fd < 0) {
+		write(STDERR_FILENO, "unable to open /flag\n", 21);
+		_exit(1);
 	}
-	read(fd, flag, 1024);
-	puts(flag);
-	chmod("/flag", 0644);
+	ssize_t n = read(fd, flag, sizeof(flag));
+	if (n > 0) {
+		write(STDOUT_FILENO, flag, (size_t)n);
+	}
 	close(fd);
-	exit(-11 & 0xff);
+	_exit(-11 & 0xff);
 }
 
 __attribute__((constructor)) void register_crash_flag_handler()
 {
-	stack_t ss;
-	ss.ss_sp = malloc(SIGSTKSZ);
-	ss.ss_size = SIGSTKSZ;
-	ss.ss_flags = 0;
-	if (sigaltstack(&ss, NULL) == -1) {
-		perror("sigaltstack");
-		exit(1);
-	}
-
 	struct sigaction sa = { 0 };
 	sa.sa_handler = handle_crash;
-	sa.sa_flags = SA_ONSTACK;
 
 	sigaction(SIGSEGV, &sa, NULL);
 	sigaction(SIGILL, &sa, NULL);
 	sigaction(SIGBUS, &sa, NULL);
+	sigaction(SIGABRT, &sa, NULL);
 }
